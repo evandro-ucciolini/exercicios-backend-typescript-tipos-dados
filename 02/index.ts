dcdacb1e2a066bed925dae7a7bbe82a128ec182e
Evandro <evandro.ucciolini@outlook.com>
2024-04-21 16:21:00 -0300

    Exercício 02 feito

---
+++ 02/index.ts
@@ -0,0 +1,52 @@
+
+const lista = [
+    {
+        nome: "Guido",
+        idade: 32,
+        status: true,
+    },
+    {
+        nome: "Dani",
+        idade: 30,
+        status: true,
+    },
+    {
+        nome: "João",
+        idade: 40,
+        status: false,
+    },
+    {
+        nome: "Guilherme",
+        idade: 29,
+        status: true,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+        status: false,
+    },
+    {
+        nome: "José",
+        idade: 28,
+        status: false,
+    },
+]
+
+const filtrarUsuarios = (usuarios: { nome: string, idade: number, status: boolean }[], usuario: string): { nome: string, idade: number, status: boolean }[] => {
+
+
+    const usuarioEncontrado = usuarios.filter((usuarioLista) => {
+
+        return usuarioLista.nome.toLowerCase().includes(usuario.toLowerCase())
+
+    })
+
+    return usuarioEncontrado
+
+}
+
+console.log(filtrarUsuarios(lista, "Jo"));
+
+
+
+
